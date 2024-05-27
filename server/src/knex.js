8158dd6c2ec49a073c47d0b614759bd4b08a975f
takumaBTC6 <takuma_suzuki_ab@mail.toytota.co.jp>
2024-05-27 13:36:45 +0900

    knexfile修正

--- server/src/knex.js
+++ server/src/knex.js
@@ -2,4 +2,5 @@ const config = require("../knexfile");
 const knex = require("knex");
 
 const environment = process.env.DATABASE_URL ? "production" : "development";
+console.log(environment);
 module.exports = knex(config[environment]);
