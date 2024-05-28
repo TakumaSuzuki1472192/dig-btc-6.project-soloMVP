dbe1f97c91de08ed18d7cac8123968a90c0acd00
takumaBTC6 <takuma_suzuki_ab@mail.toytota.co.jp>
2024-05-28 09:16:57 +0900

    本番前の状態

---
+++ server/test/items.spec.js
@@ -0,0 +1,9 @@
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+// chai.use(chaiHttp);
+// const { setupServer } = require("../src/server");
+// const { has } = require("underscore");
+// const expect = chai.expect;
+
+
+// const server = setupServer();
